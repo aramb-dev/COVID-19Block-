eed60b2d330e02b8258aa0169cae0eda46807677
aramservices198 <aramservices198@hotmail.com>
2020-11-10 21:40:35 -0500

    Version 2.0.1 of COVID-19 BLOCK!

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# COVID-19BLO
- 
+# COVID-19BLOCKER!!!
+**Let's at least give some optimism:** The endless coronavirus news doesn't help, it only enhances the fear & anxiety in us. Most of us know enough about it e.g. best practices, and the future of COVID-19 is NOT in our control (unless you're directly solving it). 
+
+The goal of this extension is to redirect your energy on things that are in your control, such as learning a new instrument, learning to code, etc... instead of being distracted by events that you can't change. Those people that are strong in brain momery and are persistent to keep going on with their goals do not make things you cannot control a problem. This is what this extension helps you do. ***Coronavirus is bad enough for our physical health, let it not degrade our mental health too. Keep being optimistic. ***
+
+🔑 *Stop focusing on what you can't change, instead load this chrme extension and enjoy corona-free browsing.*
+
+---
+**OLD Installation Instructions**
+1. Click "Clone or Download"
+2. Extract the zip file
+3. Go to chrome://extensions/
+4. Turn on Developer Mode (top right switch)
+5. Click Load unpacked
+6. Click into the extension folder
+7. Click "select folder"
+Done! Now turn the extension on & enjoy corona-free browsing!
+
---
+++ extension/script.js
@@ -0,0 +1,109 @@
+url = window.location.toString()
+
+function nukePage(){
+    document.body.remove()
+    document.title = "BLOCKED"
+}
+
+function removeYouTube(){
+    strs = document.querySelectorAll("yt-formatted-string")
+    
+    warning = document.querySelector("ytd-clarification-renderer")
+    if(warning != null){
+        warning.remove()
+    }
+    
+    for (var i = 0; i < strs.length; ++i){
+        el = strs[i]
+        str = el.innerText
+        if (str.match(/Coronavirus|Coronavirus\pandemic|Coronavirus symptoms|Coronavirus news|Coronavirus USA|Coronavirus China|Coronavirus Italy|Coronavirus recession|Coronavirus update|Coronavirus map|Coronavirus vaccine|Coronavirus pets|COVID-19|COVID-19 live updates|Pandemic|Face masks|Respirator masks|N95 face masks/gi)){
+            
+            vidResult = el.closest("ytd-video-renderer")
+            if (vidResult != null){
+                vidResult.remove()
+            }
+
+            vidResult = el.closest("ytd-rich-item-renderer")
+            if (vidResult != null){
+                vidResult.remove()
+            }
+            if (el !=null){
+                el.remove()
+            }
+        }
+        
+    }
+}
+
+function removeGoogle(){
+    strs = document.querySelectorAll(".g")
+
+    for (var i = 0; i < strs.length; ++i){
+
+        el = strs[i]
+        str = el.textContent
+        if (str.match(/Coronavirus|Coronavirus\pandemic|Coronavirus symptoms|Coronavirus news|Coronavirus USA|Coronavirus China|Coronavirus Italy|Coronavirus recession|Coronavirus update|Coronavirus map|Coronavirus vaccine|Coronavirus pets|COVID-19|COVID-19 live updates|Pandemic|Face masks|Respirator masks|N95 face masks/gi)){
+            el.remove()
+        }
+        
+    }
+    
+    strs = document.querySelectorAll(".mod")
+
+    for (var i = 0; i < strs.length; ++i){
+        el = strs[i]
+        if (str.match(/Coronavirus|Coronavirus\pandemic|Coronavirus symptoms|Coronavirus news|Coronavirus USA|Coronavirus China|Coronavirus Italy|Coronavirus recession|Coronavirus update|Coronavirus map|Coronavirus vaccine|Coronavirus pets|COVID-19|COVID-19 live updates|Pandemic|Face masks|Respirator masks|N95 face masks/gi)){
+            el.remove()
+        }
+        else{
+            str = el.textContent
+            if (str.match(/Coronavirus|Coronavirus\pandemic|Coronavirus symptoms|Coronavirus news|Coronavirus USA|Coronavirus China|Coronavirus Italy|Coronavirus recession|Coronavirus update|Coronavirus map|Coronavirus vaccine|Coronavirus pets|COVID-19|COVID-19 live updates|Pandemic|Face masks|Respirator masks|N95 face masks/gi)){
+                el.remove()
+            }
+        }
+        
+    }
+    
+    if (url.match(/Coronavirus|Coronavirus\pandemic|Coronavirus symptoms|Coronavirus news|Coronavirus USA|Coronavirus China|Coronavirus Italy|Coronavirus recession|Coronavirus update|Coronavirus map|Coronavirus vaccine|Coronavirus pets|COVID-19|COVID-19 live updates|Pandemic|Face masks|Respirator masks|N95 face masks/gi)){
+        kp_wp_tab = document.querySelectorAll(".kp-wholepage")
+        for (var i = 0; i < kp_wp_tab.length; ++i){
+            el = kp_wp_tab[i]
+            el.remove()
+        }
+
+        strs = document.querySelectorAll(".hide-focus-ring")
+        for (var i = 0; i < strs.length; ++i){
+            el = strs[i]
+            el.remove()
+        }
+    }
+    
+}
+
+function instaRemove(){
+    strs = document.querySelectorAll("a, p, h1, h2, h3, h4, h5")
+
+    for (var i = 0; i < strs.length; ++i){
+        el = strs[i]
+        str = el.textContent
+        if (str.match(/Coronavirus|Coronavirus\pandemic|Coronavirus symptoms|Coronavirus news|Coronavirus USA|Coronavirus China|Coronavirus Italy|Coronavirus recession|Coronavirus update|Coronavirus map|Coronavirus vaccine|Coronavirus pets|COVID-19|COVID-19 live updates|Pandemic|Face masks|Respirator masks|N95 face masks/gi)){
+            el.remove()
+        }
+    }
+}
+
+if(url.includes("youtube.com")){
+    window.setInterval(removeYouTube, 1000);
+}
+else if(url.includes("worldometers.info/coronavirus") || 
+    url.includes("who.int/health-topics/coronavirus")){
+    nukePage()
+}
+else if(url.includes("google")){
+    removeGoogle()
+    instaRemove()
+}
+else{
+    window.setInterval(instaRemove, 1000)
+    instaRemove()
+}
